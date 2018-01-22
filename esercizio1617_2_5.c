0a7b5c56bb8e6a47d2ccce5a1d67dbbff2035bda
Teodoro777 <teodoro.coluccio@gmail.com>
2018-01-22 13:07:28 +0100

    Soluzione esercizio1617_2_5

---
+++ esercizio1617_2_5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int X[100] = {10, 2, 3, 13, 101}; // vettore principale
+int last = 4; // indice dell'ultimo elemento utile
+float MX[100]; // nuovo vettore per le medie
+
+void media(int X[], int last, int n, float MX[]) {
+  int i, k;
+  float parziale;
+  for (i = 0; i <= last; i++) { // per quati sono gli elementi di X 
+    parziale = 0.0; // azzero ogni volta il parziale
+
+    if (i >= (n-1)) { // se i e' maggiore/uguale n-1
+      for (k = i-(n-1); k<=i; k++) parziale+= X[k]; 
+      MX[i] = parziale / n; // esegue la normale media
+    }
+    else { // se i < n -1
+      for (k = 0; k <= i; k++) parziale+= X[k];
+      MX[i] = parziale / (i+1); // esegue una media con divisore pari al numero degli elementi sommati, senza badare ad n
+    }
+  }
+}
+
+int main() {
+  int i;
+  
+  media(X, last, 3, MX);
+  
+  for(i = 0; i <= last; i++) printf("%.2f\n", MX[i]);
+  
+  return 0;
+}
+
